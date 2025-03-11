9cbe89b9977564d7590e9a3b97bb1e539e3c6ed6
AlexjkB <alexjkbaker@gmail.com>
2025-03-10 23:02:52 -0500

    progress: !help & !scores

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Bot to play trivia.
 
 Shoutout Anders. 
+
+Commands:
+
+!play  -- starts the game
+!stop  -- stops the game
+!score -- prints the scores of all the players
+!help  -- prints all the commands and what they do
+
--- bot.py
+++ bot.py
@@ -1,14 +1,33 @@
-import os
 import discord
+import os
 from dotenv import load_dotenv
+from enum import Enum
 
 load_dotenv()
 TOKEN = os.getenv("BOT_KEY")
+CLUE_API = os.getenv("CLUE")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+##################################################
+
+class State(Enum):
+    Stopped = 1
+    Playing = 2
+
+state = State.Stopped
+
+scores = {}
+
+clue = ''
+answer= ''
+category = ''
+value = 0
+
+##################################################
 
 @client.event
 async def on_ready():
@@ -16,10 +35,39 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    ## Ignore messages the bot has sent.
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    ## Display scores at any moment
+    if message.content.startswith('!score'):
+        if scores:
+            text = 'SCORES \n'
+            for player in scores:
+                text = text + player + ': ' + str(scores[player]) + '\n'
+            await message.channel.send(text)  
+        else:
+            await message.channel.send('No scores at the moment.')
+        return 
+
+    ## Display all commands at any moment 
+    if message.content.startswith('!help'):
+        text = 'ALL COMMANDS \n'
+        text += '\'!help\'    - Displays all commands. \n'
+        text += '\'!play\'    - Starts the game. \n'
+        text += '\'!stop\'    - Stops the game. \n'
+        text += '\'!scores\'  - Stops the game. \n'
+        await message.channel.send(text)
+        return 
+
+    if state = State.Playing:
+        if message.content.startswith('!stop'):
+            state = State.Stopped
+            return
+        else:
+            ''' handle answer attempts'''
+    
+    '''TODO - !play and !stop'''
 
 client.run(TOKEN)
