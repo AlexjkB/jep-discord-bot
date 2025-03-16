755a0f81e44eb7c94295b02c59a9ce2bb71d9629
AlexjkB <alexjkbaker@gmail.com>
2025-03-16 14:29:35 -0500

    answer and guess comparisons should allow for things like typos and guessing only last names

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ import requests
 import json
 from dotenv import load_dotenv
 from enum import Enum
+from itertools import combinations
+import copy
+import re
+import Levenshtein
 
 load_dotenv()
 TOKEN = os.getenv("BOT_KEY")
@@ -48,18 +52,110 @@ def new_clue():
         else:
             value = str(temp.replace('$',''))
     except:
-        print('Error here bruh')
         clue = 'Error getting clue from API.'
         answer = ''
         category = 'Error'
         value = 0
 
+# code inspired by protobowl checker2
+equivalence_list = [
+    ['zero', 'zeroeth', 'zeroth', '0'],
+    ['one', 'first', 'i', '1', '1st'],
+    ['two', 'second', 'ii', '2', '2nd'],
+    ['three', 'third', 'iii', 'turd', '3', '3rd'],
+    ['four', 'forth', 'fourth', 'iv', '4', '4th'],
+    ['fifth', 'five', '5', '5th', 'v', 'versus', 'vs', 'against'],
+    ['sixth', 'six', 'vi', 'emacs', '6', '6th'],
+    ['seventh', 'seven', 'vii', '7', '7th'],
+    ['eight', 'eighth', '8', 'viii', 'iix', '8th'],
+    ['nine', 'nein', 'ninth', 'ix', '9', '9th'],
+    ['ten', 'tenth', '10', '10th', 'x', 'by', 'times', 'product', 'multiplied', 'multiply'],
+    ['eleventh', 'eleven', 'xi', '11th'],
+    ['twelfth', 'twelveth', 'twelve', '12', 'xii', '12th'],
+    ['thirteenth', 'thirteen', '13', 'xiii', '13th'],
+    ['fourteenth', 'fourteen', 'ixv', '14th'],
+    ['fifteenth', 'fifteen', '15', 'xv', '15th'],
+    ['sixteenth', 'sixteen', '16', 'xvi', '16th'],
+    ['seventeenth', 'seventeen', '17', 'xvii', '17th'],
+    ['eighteenth', 'eighteen', 'eightteen', '18', 'xviii', '18th'],
+    ['nineteenth', 'ninteenth', 'ninteen', 'nineteen', '19', 'ixx', '19th'],
+    ['twentieth', 'twenty', 'xx', '20', '20th'],
+    ['thirtieth', 'thirty', 'xxx', '30', '30th'],
+    ['hundred', 'c', '100', '100th'],
+    ['dr', 'doctor', 'drive'],
+    ['mr', 'mister'],
+    ['ms', 'miss', 'mrs'],
+    ['st', 'saint', 'street'],
+    ['rd', 'road'],
+    ['albert', 'al'],
+    ['robert', 'bob', 'rob'],
+    ['william', 'will', 'bill'],
+    ['richard', 'rich', 'dick'],
+    ['jim', 'james'],
+    ['gregory', 'greg'],
+    ['christopher', 'chris'],
+    ['benjamin', 'ben'],
+    ['nicholas', 'nick'],
+    ['anthony', 'tony'],
+    ['lawrence', 'larry'],
+    ['edward', 'edvard', 'edouard', 'ed'],
+    ['kim', 'kimball'],
+    ['vladimir', 'vlad'],
+    ['vic','victor'],
+    ['samuel', 'samantha', 'sam'],
+    ['log', 'logarithm'],
+    ['constant', 'number'],
+]
+equivalence_map = {}
+for group in equivalence_list:
+    for item in group:
+        group_no_repeat = copy.deepcopy(group)
+        group_no_repeat.remove(item)
+        equivalence_map[item] = group_no_repeat
+
+articles = ['a','an','the']
+
+# known issue: if there are multiple words that have equivalents, will only get combinations for first word that's elgibile
+def get_all_combinations(input_list):
+    all_lists = [input_list]
+    for word in input_list:
+        if word in equivalence_map:
+            for equivalent in equivalence_map[word]:
+                copy_list = copy.deepcopy(input_list)
+                copy_list[input_list.index(word)] = equivalent
+                all_lists.append(copy_list)
+    all_combinations = []
+    for all_input_list in all_lists:
+        combo = []
+        for r in range(len(all_input_list) + 1):
+            for combination in combinations(all_input_list, r):
+                combo.append(''.join(text for text in list(combination)))
+        del combo[0]
+        for i in combo:
+            if i not in all_combinations:
+                all_combinations.append(i)
+    return all_combinations
+
+def process_string(text):
+    text = ''.join([char for char in text if char.isalnum() or char == ' '])
+    text = text.lower()
+    text = re.sub(r"\s+", " ", text)
+    text_list = text.split(' ')
+    text_list = [word for word in text_list if word not in articles]
+    return text_list
+
+def max_levenshtein(answer_options, guess):
+    max_levenshtein_ratio = 0.0
+    for option in answer_options:
+        max_levenshtein_ratio = max(max_levenshtein_ratio, Levenshtein.ratio(option, guess))
+    return max_levenshtein_ratio 
+
 def check_answer(guess):
     global answer
-    global scores
-    if guess.lower().replace(' ','') == answer.lower().replace(' ',''):
-        return True
-    return False
+    working_answer = get_all_combinations(process_string(answer))
+    working_guess = ''.join([word for word in process_string(guess)])
+    # magic number 0.9 for :
+    return max_levenshtein(working_answer, working_guess) >= 0.9
 
 ##################################################
 @tasks.loop(seconds=8.0, count=1)
@@ -72,7 +168,7 @@ async def clue_loop():
     global channel
     answer_lock = False
     new_clue()
-    text = category + ' for $' + value + '\n'
+    text = '**' + category + '** for $' + value + '\n\n'
     text += clue
     await channel.send(text)
 
@@ -88,7 +184,7 @@ async def answer_loop():
     global answer
     global channel
     answer_lock = True
-    text = 'Answer: ' + answer
+    text = 'Answer: **' + answer + '**'
     await channel.send(text)
 
 @answer_loop.after_loop
@@ -96,9 +192,6 @@ async def after_answer_loop():
     global state
     if state == State.Playing:
         clue_loop.start()
-
-
-    
 
 ##################################################
 
@@ -129,7 +222,7 @@ async def on_message(message):
     ## Display scores at any moment
     if message.content.startswith('!score'):
         if scores:
-            text = 'SCORES \n'
+            text = '**Scores** \n\n'
             for player in scores:
                 text = text + player + ': ' + str(scores[player]) + '\n'
             await message.channel.send(text)   
@@ -139,11 +232,11 @@ async def on_message(message):
 
     ## Display all commands at any moment 
     if message.content.startswith('!help'):
-        text = 'ALL COMMANDS \n'
+        text = '**All Commands**  \n\n'
         text += '\'!help\'    - Displays all commands. \n'
         text += '\'!play\'    - Starts the game. \n'
         text += '\'!stop\'    - Stops the game. \n'
-        text += '\'!scores\'  - Stops the game. \n'
+        text += '\'!scorew\'  - Displays the current scores. \n'
         await message.channel.send(text)
         return 
 
